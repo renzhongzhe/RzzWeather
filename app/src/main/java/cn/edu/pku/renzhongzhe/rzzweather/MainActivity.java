eb18f6484ea10297592d45cbd7ef1cd4df525136
renzhongzhe <renzhongzhe@qq.com>
2016-09-26 13:58:33 +0800

    init project

---
+++ app/src/main/java/cn/edu/pku/renzhongzhe/rzzweather/MainActivity.java
@@ -0,0 +1,15 @@
+package cn.edu.pku.renzhongzhe.rzzweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/9/26.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
