76da959754ef77cd210957893c527a029abf4865
renzhongzhe <renzhongzhe@qq.com>
2016-10-18 21:00:08 +0800

    init project

--- app/src/main/java/cn/edu/pku/renzhongzhe/rzzweather/MainActivity.java
+++ app/src/main/java/cn/edu/pku/renzhongzhe/rzzweather/MainActivity.java
@@ -2,10 +2,12 @@ package cn.edu.pku.renzhongzhe.rzzweather;
 
 import android.app.Activity;
 import android.app.backup.SharedPreferencesBackupHelper;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v4.content.IntentCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -33,6 +35,8 @@ public class MainActivity extends Activity implements View.OnClickListener{
     private static final int UPDATE_TODAY_WEATHER = 1;
 
     private ImageView mUpdateBtn;
+
+    private ImageView mCitySelect;
 
     private TextView cityTv, timeTv, humidityTv, weekTv, pmDataTv, pmQualityTv, temperatureTv, climateTv, windTv, city_name_Tv;
     private ImageView weatherImg, pmImg;
@@ -62,6 +66,9 @@ public class MainActivity extends Activity implements View.OnClickListener{
             Log.d("myWeather","网络挂了");
             Toast.makeText(MainActivity.this, "网络挂了", Toast.LENGTH_SHORT).show();
         }
+        mCitySelect = (ImageView)findViewById(R.id.title_city_manager);
+        mCitySelect.setOnClickListener(this);
+
         initView();
     }
     void initView(){
@@ -90,6 +97,11 @@ public class MainActivity extends Activity implements View.OnClickListener{
     }
     @Override
     public void onClick(View view){
+        if (view.getId()==R.id.title_city_manager){
+            Intent i = new Intent(this,SelectCity.class);
+            //startActivity(i);
+            startActivityForResult(i,1);
+        }
         if (view.getId()==R.id.title_update_btn){
             SharedPreferences sharedPreferences = getSharedPreferences("config",MODE_PRIVATE);
             String cityCode = sharedPreferences.getString("main_city_code","101010100");
@@ -101,6 +113,19 @@ public class MainActivity extends Activity implements View.OnClickListener{
             {
                 Log.d("myWeather", "网络挂了");
                 Toast.makeText(MainActivity.this,"网络挂了！",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            String newCityCode= data.getStringExtra("cityCode");
+            Log.d("myWeather", "选择的城市代码为"+newCityCode);
+            if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
+                Log.d("myWeather", "网络OK");
+                queryWeatherCode(newCityCode);
+            } else {
+                Log.d("myWeather", "网络挂了");
+                Toast.makeText(MainActivity.this, "网络挂了！", Toast.LENGTH_LONG).show();
             }
         }
     }
@@ -163,6 +188,7 @@ public class MainActivity extends Activity implements View.OnClickListener{
         int highCount =0;
         int lowCount=0;
         int typeCount =0;
+        int wenduCount =0;
         try {
             XmlPullParserFactory fac = XmlPullParserFactory.newInstance();
             XmlPullParser xmlPullParser = fac.newPullParser();
